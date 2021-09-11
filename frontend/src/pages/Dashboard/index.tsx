4479b6a360be9a92256f11b72178aa568f06435a
Fernando Coutinho <fa2c1@hotmail.com>
2021-09-11 15:25:20 -0300

    Routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,33 @@
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
+import DataTable from "components/DatTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+const Dashboard = () => {
+    return (
+        <>
+            <NavBar />
+            <div className="container">
+                <h1 className="text-primary py-3">Dashboars de Vendas</h1>
+                <div className="row px-3">
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secundary">Taxa de sucesso (%)</h5>
+                        <BarChart />
+                    </div>
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secundary">Todas vendas</h5>
+                        <DonutChart />
+                    </div>
+                </div>
+                <div className="row px-3">
+                    <h2 className="text-primary py-3">Todas vendas</h2>
+                </div>
+                <DataTable />
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default Dashboard;
